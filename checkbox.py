3ce0926dff33bc4b6a848c755442236a6b538def
RoshiniFernando <47807830+RoshiniFernando@users.noreply.github.com>
2023-01-04 22:13:34 +0530

    Added the checkboxes tried file

---
+++ checkbox.py
@@ -0,0 +1,22 @@
+# Import streamlit
+import streamlit as st
+
+# Header text
+st.header('st.checkbox')
+
+st.write ('What would you like to order?')
+
+# Check boxes
+icecream = st.checkbox('Ice cream')
+coffee = st.checkbox('Coffee')
+cola = st.checkbox('Cola')
+
+# Display selected 
+if icecream:
+     st.write("Great! Here's some more 🍦")
+
+if coffee: 
+     st.write("Okay, here's some coffee ☕")
+
+if cola:
+     st.write("Here you go 🥤")
